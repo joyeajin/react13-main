1892a3af046a6a7903d64ba4540d90a8721bb53a
joyeajin <dpwls31200@gmail.com>
2022-07-11 14:31:17 +0900

    라우팅연습

--- src/route/RedirectsRoute.js
+++ src/route/RedirectsRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import User from '../pages/User';
 
 const RedirectsRoute = ({auth}) => {
-    return auth === true ? <User/> : <Navigate to '/login' /> ;
+    return auth === true ? <User/> : <Navigate to= '/login' /> ;
 };
 
 export default RedirectsRoute;
